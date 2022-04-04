abc60e09365ea705bb506b52a81c74c2a9c86f47
puvistyne <puvistien@gmail.com>
2022-04-04 21:04:15 +0200

    client

---
+++ src/fr/upem/net/tcp/chatfusion/Client.java
@@ -0,0 +1,7 @@
+package fr.upem.net.tcp.chatfusion;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("This is the beginning of the new era...");
+    }
+}
