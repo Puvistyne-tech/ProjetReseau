67ffb9ce3e363e92fb7b73a7129ad8e4b90dc337
puvistyne <puvistien@gmail.com>
2022-04-06 20:14:50 +0200

    Changes by Amyr

--- src/fr/upem/net/tcp/chatfusion/utils/Commander.java
+++ src/fr/upem/net/tcp/chatfusion/utils/Commander.java
@@ -1,14 +1,43 @@
 package fr.upem.net.tcp.chatfusion.utils;
 
 import fr.upem.net.tcp.chatfusion.packet.Packet;
+import fr.upem.net.tcp.chatfusion.packet.PrivateFilePacket;
+import fr.upem.net.tcp.chatfusion.packet.PrivateMessagePacket;
 import fr.upem.net.tcp.chatfusion.packet.PublicMessagePacket;
 
+import java.util.regex.Pattern;
+
 public class Commander {
-    static private final String PUBLIC = "", PRIVE = "DM", CONNEXION_PRIVE = "/@", OK_REQUEST = "ACC", KO_REQUEST = "REF";
+    //static private final String PUBLIC = "", PRIVE = "DM", CONNEXION_PRIVE = "/@", OK_REQUEST = "ACC", KO_REQUEST = "REF";
+    static private final String PATTERN_MESSAGE = "([/|@]?)([A-Za-z0-9_]*):([A-Za-z0-9_]*)\s+(.*)";
 
-    public static Packet commande(String string){
+    // public static Packet commande(String inpupubl
+    static Packet commande(String input) {
+        var pattern = Pattern.compile(PATTERN_MESSAGE);
+        var matcher = pattern.matcher(input);
 
-        return new PublicMessagePacket("server","login",string);
+        var loginDest = matcher.group(2);
+        var serverDest = matcher.group(3);
+
+        switch (matcher.group(1)) {
+            case "@" :
+                // Envoie d'un message privé
+                //return new PrivateMessagePacket(server, login, )
+                break;
+            case "/" :
+                // Envoie d'un fichier (privé)
+
+                return new PrivateFilePacket();
+            default :
+                // Un message publique
+                var textMessage = matcher.group(4);
+                //return new PublicMessagePacket();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
 
