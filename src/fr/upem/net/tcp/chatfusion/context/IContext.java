6fba90272f3182ded275898e805111ec0da055d0
puvistyne <56639092+Puvistyne-tech@users.noreply.github.com>
2022-04-07 17:53:44 +0200

    Add files via upload

---
+++ src/fr/upem/net/tcp/chatfusion/context/IContext.java
@@ -0,0 +1,25 @@
+package fr.upem.net.tcp.chatfusion.context;
+
+import fr.upem.net.tcp.chatfusion.packet.Packet;
+
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+
+public interface IContext {
+
+    int BUFFER_SIZE = 1_024;
+    Charset CHARSET = StandardCharsets.UTF_8;
+
+    void processIn() throws Exception;
+    void processOut();
+    void queueMessage(Packet packet);
+    void updateInterestOps();
+    void doRead() throws Exception;
+    void doWrite() throws IOException;
+    void silentlyClose();
+
+
+}
